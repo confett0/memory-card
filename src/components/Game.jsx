8d696dffd2063a98daf21d4a24c214341151f898
confett0 <118047961+confett0@users.noreply.github.com>
2024-06-10 17:28:50 +0200

    Create handleClick function for card elements

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,32 +1,44 @@
 import { useEffect, useState } from "react";
 import shuffleArray from "./shuffleArray";
-import Card from "./Card.jsx"
+import Card from "./Card.jsx";
 
 export default function Game() {
+  const [charactersData, setCharactersData] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [clickedCardIds, setClickedCardIds] = useState([]);
 
-    const [charactersData, setCharactersData] = useState([])
+  useEffect(() => {
+    fetch("https://potterapi-fedeperin.vercel.app/en/characters")
+      .then((response) => response.json())
+      .then((data) => setCharactersData(data));
+  }, []);
 
-    useEffect(() => {
-        fetch("https://potterapi-fedeperin.vercel.app/en/characters")
-        .then(response => response.json())
-        .then(data => setCharactersData(data))
-    }, [])
+  const shuffledCharacters = shuffleArray(charactersData);
 
-    const shuffledCharacters = shuffleArray(charactersData);
+  const handleClick = (cardId) => {
+    if (!clickedCardIds.includes(cardId)) {
+      if (clickedCardIds.length === 24) {
+        alert("You win")
+      }
+      setClickedCardIds(prevIds => [...prevIds, cardId])
+      setCurrentScore((prevScore) => prevScore + 1);
+      
+    } else {
+      
+      alert("You lost")
+    }
+    
+  };
 
-    const cardElements = shuffledCharacters.map((char) => (
-        <Card
-          key={char.index}
-          index={char.index}
-          imgUrl={char.image}
-          name={char.nickname}
-          
-        />
-      ));
+  const cardElements = shuffledCharacters.map((char) => (
+    <Card
+      key={char.index}
+      index={char.index}
+      imgUrl={char.image}
+      name={char.nickname}
+      handleClick={handleClick}
+    />
+  ));
 
-    return (
-        <div className="game-container">
-        {cardElements}
-        </div>
-    )
+  return <div className="game-container">{cardElements}</div>;
 }
