4eebf660167f5cb16f3afce5a96dd1f71a42c483
confett0 <118047961+confett0@users.noreply.github.com>
2024-06-10 17:12:42 +0200

    Fetch characters data from api

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
 
-  const [isGameOn, setIsGameOn] = useState(false)
+  const [isGameOn, setIsGameOn] = useState(true)
 
   return (
     <>
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react";
+
 export default function Game() {
+
+    const [charactersData, setCharactersData] = useState([])
+
+    useEffect(() => {
+        fetch("https://potterapi-fedeperin.vercel.app/en/characters")
+        .then(response => response.json())
+        .then(data => setCharactersData(data))
+    }, [])
+
+    console.log(charactersData)
+
     return (
         <h1>Game is on!</h1>
     )
