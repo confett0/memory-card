1dd9f954d06355016940b4a907c3e082a6855473
confett0 <118047961+confett0@users.noreply.github.com>
2024-06-21 21:33:26 +0200

    Create Modal component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,8 @@
+export default function Modal({title}) {
+    return (
+        <div className="modal">
+            <h3>title</h3>
+            <button>Play again?</button>
+        </div>
+    )
+}
