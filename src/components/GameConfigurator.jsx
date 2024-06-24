e042616db9ed3fa54a674c1a841c2bc930c4e692
confett0 <118047961+confett0@users.noreply.github.com>
2024-06-24 18:06:55 +0200

    Style level buttons

--- src/components/GameConfigurator.jsx
+++ src/components/GameConfigurator.jsx
@@ -3,18 +3,34 @@ export default function GameConfigurator({ house, chooseHouse, startGame }) {
     <>
       {!house ? (
         <div className="house-wrap">
-          <h3>Pick your Hogwards house</h3>
-          <button onClick={() => chooseHouse("gryffindor")}><img src="./gryffindor-button.png"/></button>
-          <button onClick={() => chooseHouse("slytherin")}><img src="./slytherin-button.png"/></button>
-          <button onClick={() => chooseHouse("ravenclaw")}><img src="./ravenclaw-button.png"/></button>
-          <button onClick={() => chooseHouse("hufflepuff")}><img src="./hufflepuff-button.png"/></button>
+          <h3>Select your Hogwarts house</h3>
+          <button onClick={() => chooseHouse("gryffindor")}>
+            <img className="house-button" src="./gryffindor-button.png" />
+          </button>
+          <button onClick={() => chooseHouse("slytherin")}>
+            <img className="house-button" src="./slytherin-button.png" />
+          </button>
+          <button onClick={() => chooseHouse("ravenclaw")}>
+            <img className="house-button" src="./ravenclaw-button.png" />
+          </button>
+          <button onClick={() => chooseHouse("hufflepuff")}>
+            <img className="house-button" src="./hufflepuff-button.png" />
+          </button>
         </div>
       ) : (
         <div className="level-wrap">
-          <h3>Pick your level</h3>
-          <button onClick={() => startGame("easy")}>Easy</button>
-          <button onClick={() => startGame("medium")}>Medium</button>
-          <button onClick={() => startGame("hard")}>Hard</button>
+          <h3>Select difficulty</h3>
+          <div className="level-button-container">
+            <button className="level-button" onClick={() => startGame("easy")}>
+              Easy
+            </button>
+            <button className="level-button" onClick={() => startGame("medium")}>
+              Medium
+            </button>
+            <button className="level-button" onClick={() => startGame("hard")}>
+              Hard
+            </button>
+          </div>
         </div>
       )}
     </>
