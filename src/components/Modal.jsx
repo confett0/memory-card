7fcabb8a28b00caf27b946bab66b8692cde1ebf8
confett0 <118047961+confett0@users.noreply.github.com>
2024-06-26 17:34:27 +0200

    Add different styling and content for winning/losing modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,27 @@
 import "../Modal.css";
 
 export default function Modal({ setIsOpen, gameResult, setIsGameOn }) {
+    const winData = {
+        title: "You win",
+        subtitle: "10 point to team",
+        backgroundUrl: "./win.jpg"
+    }
+
+    const loseData = {
+        title: "You lose",
+        subtitle: "Minus 10 point to team",
+        backgroundUrl: "./lose.jpg"
+    }
+
+    let data = gameResult === "win" ? winData : loseData
+    console.log(data.backgroundUrl)
+
   return (
     <>
       <div className="darkBG" />
-      <div className="modal">
-        <h3>{gameResult === "win" ? "You win" : "You lose"}</h3>
+      <div className="modal" style={{background: `url(${data.backgroundUrl})`}}>
+        <h3>{data.title}</h3>
+        <h5>{data.subtitle}</h5>
         <button onClick={() => setIsOpen(false)}>Play again?</button>
         <button onClick={() => setIsGameOn(false)}>Quit</button>
       </div>
